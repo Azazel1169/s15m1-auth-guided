6106a2731ab55c4a10448bdd6b97b6b75ce0e432
Josh Rime <joshrime1990@icloud.com>
2025-04-16 22:15:07 -0500

    Added resetdb to package.json

--- api/auth/auth-router.js
+++ api/auth/auth-router.js
@@ -1,31 +1,30 @@
-const express = require('express')
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const router = express.Router();
+const User = require("../users/users-model");
 
-const router = express.Router()
+router.post("/register", async (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    const hash = bcrypt.hashSync(password, 12);
+    const newUser = { username, password: hash };
+    const result = await User.add(newUser);
+    res.status(201).json({
+      message: `Nice to have you, ${result.username}`,
+    });
+  } catch (err) {
+    next(err);
+  }
+});
+router.post("/login", async (req, res, next) => {
+  res.json({
+    message: "login working",
+  });
+});
+router.get("/logout", async (req, res, next) => {
+  res.json({
+    message: "logout working",
+  });
+});
 
-
-
-router.post('/register', async  (req,res,next) => {
-    res.json({
-        message:'register working'
-    })
-
-})
-router.post('/login', async  (req,res,next) => {
-    res.json({
-        message:'login working'
-    })
-
-})
-router.get('/logout', async  (req,res,next) => {
-    res.json({
-        message:'logout working'
-    })
-
-})
-
-
-
-
-
-
-module.exports = router
+module.exports = router;
